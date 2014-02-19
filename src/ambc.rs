7d6abc9ff5e7f8453c0413381ae5d81a193bc771
Maciek Makowski <maciek.makowski@gmail.com>
2014-02-19 22:38:40 +0000

    added block body

--- src/ambc.rs
+++ src/ambc.rs
@@ -1,7 +1,7 @@
 extern mod rustc = "rustc#0.9";
 
 use rustc::lib::llvm::llvm;
-use rustc::lib::llvm::{Bool, False, True};
+use rustc::lib::llvm::{False};
 
 fn main() {
     unsafe {
@@ -12,10 +12,14 @@ fn main() {
         });
 
         let builder = llvm::LLVMCreateBuilderInContext(llvm_context);
-        let double_type = llvm::LLVMDoubleTypeInContext(llvm_context);
-        let func_type = llvm::LLVMFunctionType(double_type, [double_type].as_ptr(), 1, False);
+        let int_type = llvm::LLVMIntTypeInContext(llvm_context, 64);
+        let func_type = llvm::LLVMFunctionType(int_type, [int_type].as_ptr(), 1, False);
         let func = "f1".with_c_str(|buf| { llvm::LLVMAddFunction(llvm_module, buf, func_type) });
-        
+        let block = "f1_start".with_c_str(|buf| { llvm::LLVMAppendBasicBlockInContext(llvm_context, func, buf) });
+        llvm::LLVMPositionBuilderAtEnd(builder, block);
+        let ret_val = llvm::LLVMConstInt(int_type, 42, False);
+        llvm::LLVMBuildRet(builder, ret_val);
+
         llvm::LLVMDumpModule(llvm_module);
     }
 }
