ed04836f5689619240d946168bb5f4815b30713b
Maciek Makowski <maciek.makowski@gmail.com>
2014-02-23 23:43:34 +0000

    cleanup: removed redundante returns

--- src/untyped_lambda.rs
+++ src/untyped_lambda.rs
@@ -26,25 +26,25 @@ fn lex(input: &str) -> ~[Token] {
         }
         pos += 1
     }
-    return tokens
+    tokens
 }
 
 fn read_space(input: &str, pos: &uint) -> Token {
     // TODO: read until char that is not space or end of input
-    return TSpace
+    TSpace
 }
 
 fn read_name(input: &str, pos: &uint) -> Token {
     // TODO: read until char that is not part of name
-    return TName(input.slice_chars(*pos, *pos + 1).to_owned())
+    TName(input.slice_chars(*pos, *pos + 1).to_owned())
 }
 
 pub fn parse(input: &str) -> ~Term {
-    return mk_term(lex(input))
+    mk_term(lex(input))
 }
 
 fn mk_term(tokens: &[Token]) -> ~Term {
-    return match tokens.head() {
+    match tokens.head() {
         &TLambda  => mk_abs(tokens),
         &TName(_) => mk_var_or_app(tokens),
         other     => fail!(format!("expected a lambda or a name but got {:?}", *other))
@@ -52,22 +52,23 @@ fn mk_term(tokens: &[Token]) -> ~Term {
 }
 
 fn mk_abs(tokens: &[Token]) -> ~Term {
-   return ~Abs(get_name(&tokens[1]), mk_term(tokens.slice_from(3)))
+   ~Abs(get_name(&tokens[1]), mk_term(tokens.slice_from(3)))
 }
 
 fn mk_var_or_app(tokens: &[Token]) -> ~Term {
     let first_term = ~Var(get_name(tokens.head()));
-    return if tokens.len() == 1 { first_term } 
-           else {
-                match &tokens[1] {
-                    &TSpace => ~App(first_term, mk_term(tokens.slice_from(2))),
-                    other   => fail!(format!("expected a whitespace but got {:?}", *other))
-                }
-           }
+    if tokens.len() == 1 { 
+        first_term 
+    } else {
+        match &tokens[1] {
+            &TSpace => ~App(first_term, mk_term(tokens.slice_from(2))),
+            other   => fail!(format!("expected a whitespace but got {:?}", *other))
+        }
+    }
 }
 
 fn get_name(token: &Token) -> ~str {
-    return match token {
+    match token {
         &TName(ref n) => n.to_owned(),
         other         => fail!(format!("expected a name but got {:?}", *other))
     }
