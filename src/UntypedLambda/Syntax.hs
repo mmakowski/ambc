b0a7db3ed4720d2883931074bbb08b5e550cc6ab
Maciek Makowski <maciek.makowski@gmail.com>
2014-03-13 12:44:29 +0000

    simplified Show for Term

--- src/UntypedLambda/Syntax.hs
+++ src/UntypedLambda/Syntax.hs
@@ -15,5 +15,4 @@ data Term = Var String
 instance Show Term where
   show (Var ident)                     = ident
   show (Abs ident term)                = "(\\" ++ ident ++ "." ++ (show term) ++ ")"
-  show (App (Var ident1) (Var ident2)) = "(" ++ ident1 ++ " " ++ ident2 ++ ")"
-  show (App term1 term2)               = "(" ++ (show term1) ++ (show term2) ++ ")"
+  show (App term1 term2)               = "(" ++ (show term1) ++ " " ++ (show term2) ++ ")"
